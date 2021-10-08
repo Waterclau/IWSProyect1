735cdfb17932349d0b76eee436d49663b82dedd3
clux <201907102@alu.comillas.edu>
2021-10-08 17:53:12 +0200

    Ventana bonita

--- src/icai/dtc/isw/client/Ventana.java
+++ src/icai/dtc/isw/client/Ventana.java
@@ -16,48 +16,112 @@ public class Ventana extends JFrame
 
     JButton btnShowAll;
 
-/*
+
     public static void main(String args[])
-	{
-		new Ventana();
-	}
-    */
+    {
+        new Ventana();
+    }
 
 
 
 
 
 
-    public Ventana(Client client)
+
+    public Ventana()
     {
         super("Ventana");
         this.setLayout(new BorderLayout());
 
+
         btnShowAll = new JButton("Mostrar todos ");
+        JLabel lblTitulo = new JLabel("PhoneFitter");
+        lblTitulo.setFont(new Font("URIAL FONT", Font.BOLD, 90));
+        JLabel lblCuestion1 = new JLabel("Marca  ");
+        lblCuestion1.setFont(new Font("URIAL FONT", Font.BOLD, 30));
+        JLabel lblCuestion2 = new JLabel("Precio: ");
+        lblCuestion2.setFont(new Font("URIAL FONT", Font.BOLD, 30));
+        JLabel lblCuestion3 = new JLabel("Modelo: ");
+        lblCuestion3.setFont(new Font("URIAL FONT", Font.BOLD, 30));
+        JLabel lblCuestion4 = new JLabel("Almacenamiento: ");
+        lblCuestion4.setFont(new Font("URIAL FONT", Font.BOLD, 30));
+        JLabel lblCuestion5 = new JLabel("Memoria: ");
+        lblCuestion5.setFont(new Font("URIAL FONT", Font.BOLD, 30));
 
 
+        JTextField txtSeleccion1 = new JTextField();
+        JTextField txtSeleccion2 = new JTextField();
+        JTextField txtSeleccion3 = new JTextField();
+        JTextField txtSeleccion4 = new JTextField();
+        JTextField txtSeleccion5 = new JTextField();
+
+
+
+        JPanel pnlCuestionario = new JPanel(new GridLayout(1,2));
+        JPanel pnlCuestiones = new JPanel(new GridLayout(5,1));
+        JPanel pnlSeleciones = new JPanel(new GridLayout(5,1));
         JPanel pnlNorte = new JPanel(new FlowLayout());
+        JPanel pnlSur= new JPanel(new FlowLayout());
+        //JLabel lblFondo = new JLabel(new ImageIcon("C:\\Users\\Claudio Esteban\\1\\POO\\pf\\ui\\Phones1.jpg"));
 
-        pnlNorte.add(btnShowAll);
+
+        pnlNorte.add(lblTitulo);
+        pnlNorte.setBackground(new Color(173,216,230));
+        /*btnShowAll.setOpaque(false);
+        btnShowAll.setContentAreaFilled(false);
+        btnShowAll.setBorderPainted(false);*/
+        pnlSur.add(btnShowAll);
+        pnlSur.setBackground(new Color(173,216,230));
+
+        pnlCuestiones.add(lblCuestion1);
+        pnlCuestiones.add(lblCuestion2);
+        pnlCuestiones.add(lblCuestion3);
+        pnlCuestiones.add(lblCuestion4);
+        pnlCuestiones.add(lblCuestion5);
+        pnlCuestiones.setBackground(new Color(173,216,230));
+
+
+        pnlSeleciones.add(txtSeleccion1);
+        pnlSeleciones.add(txtSeleccion2);
+        pnlSeleciones.add(txtSeleccion3);
+        pnlSeleciones.add(txtSeleccion4);
+        pnlSeleciones.add(txtSeleccion5);
 
 
 
+
+        pnlCuestionario.add(pnlCuestiones);
+        pnlCuestionario.add(pnlSeleciones);
+
+
+
+
+
+
+
+
+        /*
         btnShowAll.addMouseListener(new MouseAdapter()
         {
             public void mouseClicked(MouseEvent e)
             {
                 new VentanaResultados(client);
             }
-        });
-
+        });*/
 
 
 
 
 
         this.add(pnlNorte, BorderLayout.NORTH);
+        this.add(pnlSur, BorderLayout.SOUTH);
+        this.add(pnlCuestionario, BorderLayout.CENTER);
+        //this.setContentPane(new JLabel(new ImageIcon("C:\\Users\\Claudio Esteban\\1\\POO\\pf\\ui\\Phones.jpg")));
         this.pack();
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
+
 }
+
