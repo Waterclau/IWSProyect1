6cd09263132098f12ad58372e653f0e11dca6d0b
NCSanto <91725797+NCSanto@users.noreply.github.com>
2021-10-14 18:11:43 +0200

    Add files via upload

--- src/icai/dtc/isw/controler/CustomerControler.java
+++ src/icai/dtc/isw/controler/CustomerControler.java
@@ -1,13 +1,24 @@
 package icai.dtc.isw.controler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import icai.dtc.isw.dao.CustomerDAO;
 import icai.dtc.isw.domain.Customer;
+import icai.dtc.isw.domain.Movil;
+import icai.dtc.isw.client.VentanaResultados;
+
 
 public class CustomerControler {
 
 	public void getCustomer(ArrayList<Customer> lista) {
 		CustomerDAO.getClientes(lista);
-	}	
+	}
+
+	public void getMovil(ArrayList<Movil> lista, String marca, String precio, String modelo, String almacenamiento, String memoria)
+	{
+		CustomerDAO.getMoviles(lista, marca, precio, modelo, almacenamiento, memoria);
+	}
+
+
 }
