127add9da6e453ce0a4e7bc45de15267543f3e8d
clux <201907102@alu.comillas.edu>
2021-11-11 16:25:16 +0100

    Entrega1.1

--- src/icai/dtc/isw/client/Ventana.java
+++ src/icai/dtc/isw/client/Ventana.java
@@ -40,19 +40,69 @@ public class Ventana extends JFrame
 
 
 
-        btnShowAll = new JButton("Buscar ");
+        btnShowAll = new JButton("Mostrar todos ");
+        btnShowAll.setBackground(new Color(255, 170, 0));
+        btnShowAll.setForeground(new Color(255,69,0));
+        btnShowAll.setFont(new Font("URIAL FONT", Font.BOLD, 20));
         JLabel lblTitulo = new JLabel("PhoneFitter");
         lblTitulo.setFont(new Font("URIAL FONT", Font.BOLD, 90));
-        JLabel lblCuestion1 = new JLabel("Marca preferida: ");
-        lblCuestion1.setFont(new Font("URIAL FONT", Font.BOLD, 30));
-        JLabel lblCuestion2 = new JLabel("Precio máximo: ");
-        lblCuestion2.setFont(new Font("URIAL FONT", Font.BOLD, 30));
+        lblTitulo.setForeground(new Color(255,69,0));
+        JLabel lblCuestion1 = new JLabel("Marca  ");
+        lblCuestion1.setFont(new Font("URIAL FONT", Font.BOLD, 20));
+        lblCuestion1.setForeground(new Color(255,69,0));
+        lblCuestion1.setOpaque(false);
+        JLabel lblCuestion2 = new JLabel("Precio: ");
+        lblCuestion2.setFont(new Font("URIAL FONT", Font.BOLD, 20));
+        lblCuestion2.setForeground(new Color(255,69,0));
         JLabel lblCuestion3 = new JLabel("Modelo: ");
-        lblCuestion3.setFont(new Font("URIAL FONT", Font.BOLD, 30));
-        JLabel lblCuestion4 = new JLabel("Almacenamiento mínimo: ");
-        lblCuestion4.setFont(new Font("URIAL FONT", Font.BOLD, 30));
-        JLabel lblCuestion5 = new JLabel("Memoria mínima: ");
-        lblCuestion5.setFont(new Font("URIAL FONT", Font.BOLD, 30));
+        lblCuestion3.setFont(new Font("URIAL FONT", Font.BOLD, 20));
+        lblCuestion3.setForeground(new Color(255,69,0));
+        JLabel lblCuestion4 = new JLabel("Almacenamiento: ");
+        lblCuestion4.setFont(new Font("URIAL FONT", Font.BOLD, 20));
+        lblCuestion4.setForeground(new Color(255,69,0));
+        JLabel lblCuestion5 = new JLabel("Memoria: ");
+        lblCuestion5.setFont(new Font("URIAL FONT", Font.BOLD, 20));
+        lblCuestion5.setForeground(new Color(255,69,0));
+        JLabel nombreuser = new JLabel("Nombre usuario");
+        nombreuser.setFont(new Font("URIAL FONT", Font.BOLD, 10));
+        nombreuser.setForeground(new Color(255,69,0));
+        nombreuser.setBackground(new Color(255, 170, 0));
+        JToggleButton obligatorio1 = new JToggleButton("Marcar Obligatorio");
+        obligatorio1.setPreferredSize(new Dimension(20, 20));
+        obligatorio1.setForeground(new Color(255,69,0));
+        obligatorio1.setBackground(new Color(255, 170, 0));
+        obligatorio1.setFont(new Font("URIAL FONT", Font.BOLD, 10));
+
+        JButton btnMostrarUser = new JButton("Acceder al perfil ");
+        btnMostrarUser.setBackground(new Color(255, 170, 0));
+        btnMostrarUser.setForeground(new Color(255,69,0));
+        btnMostrarUser.setFont(new Font("URIAL FONT", Font.BOLD, 10));
+
+        JToggleButton obligatorio2 = new JToggleButton("Marcar Obligatorio");
+        obligatorio2.setPreferredSize(new Dimension(20, 20));
+        obligatorio2.setForeground(new Color(255,69,0));
+        obligatorio2.setBackground(new Color(255, 170, 0));
+        obligatorio2.setFont(new Font("URIAL FONT", Font.BOLD, 10));
+
+        JToggleButton obligatorio3 = new JToggleButton("Marcar Obligatorio");
+        obligatorio3.setPreferredSize(new Dimension(20, 20));
+        obligatorio3.setForeground(new Color(255,69,0));
+        obligatorio3.setBackground(new Color(255, 170, 0));
+        obligatorio3.setFont(new Font("URIAL FONT", Font.BOLD, 10));
+
+        JToggleButton obligatorio4 = new JToggleButton("Marcar Obligatorio");
+        obligatorio4.setPreferredSize(new Dimension(20, 20));
+        obligatorio4.setForeground(new Color(255,69,0));
+        obligatorio4.setBackground(new Color(255, 170, 0));
+        obligatorio4.setFont(new Font("URIAL FONT", Font.BOLD, 10));
+
+        JToggleButton obligatorio5 = new JToggleButton("Marcar Obligatorio");
+        obligatorio5.setPreferredSize(new Dimension(20, 20));
+        obligatorio5.setForeground(new Color(255,69,0));
+        obligatorio5.setBackground(new Color(255, 170, 0));
+        obligatorio5.setFont(new Font("URIAL FONT", Font.BOLD, 10));
+
+
 
 
         JTextField txtSeleccion1 = new JTextField();
@@ -61,43 +111,147 @@ public class Ventana extends JFrame
         JTextField txtSeleccion4 = new JTextField();
         JTextField txtSeleccion5 = new JTextField();
 
-
-
-        JPanel pnlCuestionario = new JPanel(new GridLayout(1,2));
+        JLabel background = new JLabel(new ImageIcon("/home/clux/IdeaProjects/projectISW19/src/icai/dtc/isw/client/fondo_phone.jpg"));
+        JLabel fondologo = new JLabel(new ImageIcon("/home/clux/IdeaProjects/projectISW19/src/icai/dtc/isw/client/fondo_titulo.jpg"));
+        //JLabel logo = new JLabel(new ImageIcon("\\home\\clux\\Desktop\\Fotos_IW\\Logo_ISW.JPG"));
+        JLabel userfoto = new JLabel(new ImageIcon("/home/clux/IdeaProjects/projectISW19/src/icai/dtc/isw/client/avatar_usuario.jpg"));
+        JLabel vacio = new JLabel();
+        vacio.setBackground(new Color(255, 170, 0));
+        JPanel usuario = new JPanel(new FlowLayout());
+        JPanel pnlCuestionario = new JPanel(new GridLayout(1,3));
         JPanel pnlCuestiones = new JPanel(new GridLayout(5,1));
         JPanel pnlSeleciones = new JPanel(new GridLayout(5,1));
+        JPanel pnlObligaciones = new JPanel(new GridLayout(5,1));
         JPanel pnlNorte = new JPanel(new FlowLayout());
         JPanel pnlSur= new JPanel(new FlowLayout());
-        //JLabel lblFondo = new JLabel(new ImageIcon("C:\\Users\\Claudio Esteban\\1\\POO\\pf\\ui\\Phones1.jpg"));
+        JPanel pnlCentro= new JPanel(new FlowLayout());
+        JPanel pnlUser = new JPanel(new GridLayout(2,1));
+        pnlUser.setBackground(new Color(255, 170, 0));
+        background.setLayout(new FlowLayout());
+        fondologo.setLayout(new FlowLayout());
 
 
-        pnlNorte.add(lblTitulo);
-        pnlNorte.setBackground(new Color(173,216,230));
-        /*btnShowAll.setOpaque(false);
-        btnShowAll.setContentAreaFilled(false);
-        btnShowAll.setBorderPainted(false);*/
+        pnlNorte.setLayout(new FlowLayout());
+        usuario.add(userfoto);
+        pnlUser.add(nombreuser);
+        pnlUser.add(btnMostrarUser);
+        usuario.add(pnlUser);
+        usuario.setBackground(new Color(255, 170, 0));
+        fondologo.add(lblTitulo);
+        fondologo.add(usuario);
+        pnlNorte.add(fondologo);
+
+        pnlNorte.setBackground(new Color(239, 127, 26));
         pnlSur.add(btnShowAll);
-        pnlSur.setBackground(new Color(173,216,230));
+        pnlSur.setBackground(new Color(239, 127, 26));
 
         pnlCuestiones.add(lblCuestion1);
         pnlCuestiones.add(lblCuestion2);
         pnlCuestiones.add(lblCuestion3);
         pnlCuestiones.add(lblCuestion4);
         pnlCuestiones.add(lblCuestion5);
-        pnlCuestiones.setBackground(new Color(173,216,230));
+        pnlCuestiones.setBackground(new Color(255, 170, 0));
+
 
 
         pnlSeleciones.add(txtSeleccion1);
+        pnlSeleciones.setBackground(new Color(255, 170, 0));
         pnlSeleciones.add(txtSeleccion2);
         pnlSeleciones.add(txtSeleccion3);
         pnlSeleciones.add(txtSeleccion4);
         pnlSeleciones.add(txtSeleccion5);
+
+        pnlObligaciones.add(obligatorio1);
+        pnlObligaciones.add(obligatorio2);
+        pnlObligaciones.add(obligatorio3);
+        pnlObligaciones.add(obligatorio4);
+        pnlObligaciones.add(obligatorio5);
+        pnlObligaciones.setBackground(new Color(255, 170, 0));
+
+
+
 
 
 
 
         pnlCuestionario.add(pnlCuestiones);
         pnlCuestionario.add(pnlSeleciones);
+        pnlCuestionario.add(pnlObligaciones);
+        //pnlCuestionario.setPreferredSize(new Dimension(100, 100));
+        background.add(pnlCuestionario);
+        pnlCentro.add(background);
+        pnlCentro.setBackground(new Color(239, 127, 26));
+
+        obligatorio1.addItemListener(new ItemListener()
+        {
+            @Override
+            public void itemStateChanged(ItemEvent itemEvent) {
+                int estado = itemEvent.getStateChange();
+                if(estado == ItemEvent.SELECTED){
+                    //label1.setText("Botón seleccionado");
+                } else {
+                    //label1.setText("Botón deseleccionado");
+                }
+            }
+        });
+
+        obligatorio2.addItemListener(new ItemListener()
+        {
+            @Override
+            public void itemStateChanged(ItemEvent itemEvent) {
+                int estado = itemEvent.getStateChange();
+                if(estado == ItemEvent.SELECTED){
+                    //label1.setText("Botón seleccionado");
+                } else {
+                    //label1.setText("Botón deseleccionado");
+                }
+            }
+        });
+
+
+        obligatorio3.addItemListener(new ItemListener()
+        {
+            @Override
+            public void itemStateChanged(ItemEvent itemEvent) {
+                int estado = itemEvent.getStateChange();
+                if(estado == ItemEvent.SELECTED){
+                    //label1.setText("Botón seleccionado");
+                } else {
+                    //label1.setText("Botón deseleccionado");
+                }
+            }
+        });
+
+        obligatorio4.addItemListener(new ItemListener()
+        {
+            @Override
+            public void itemStateChanged(ItemEvent itemEvent) {
+                int estado = itemEvent.getStateChange();
+                if(estado == ItemEvent.SELECTED){
+                    //label1.setText("Botón seleccionado");
+                } else {
+                    //label1.setText("Botón deseleccionado");
+                }
+            }
+        });
+
+        obligatorio5.addItemListener(new ItemListener()
+        {
+            @Override
+            public void itemStateChanged(ItemEvent itemEvent) {
+                int estado = itemEvent.getStateChange();
+                if(estado == ItemEvent.SELECTED){
+                    //label1.setText("Botón seleccionado");
+                } else {
+                    //label1.setText("Botón deseleccionado");
+                }
+            }
+        });
+
+
+
+
+
 
 
 
@@ -164,13 +318,16 @@ public class Ventana extends JFrame
 
 
 
+        //this.setPreferredSize(new Dimension(800, 600));
         this.add(pnlNorte, BorderLayout.NORTH);
         this.add(pnlSur, BorderLayout.SOUTH);
-        this.add(pnlCuestionario, BorderLayout.CENTER);
+        this.add(pnlCentro, BorderLayout.CENTER);
         //this.setContentPane(new JLabel(new ImageIcon("C:\\Users\\Claudio Esteban\\1\\POO\\pf\\ui\\Phones.jpg")));
         this.pack();
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
 
 
--- src/icai/dtc/isw/client/VentanaMenu.java
+++ src/icai/dtc/isw/client/VentanaMenu.java
@@ -22,17 +22,37 @@ public class VentanaMenu extends JFrame
         this.setLayout(new BorderLayout());
 
         JLabel lblUsuario = new JLabel("Usuario: ");
+        lblUsuario.setFont(new Font("URIAL FONT", Font.BOLD, 13));
+        //lblUsuario.setForeground(new Color(255, 170, 0));
         JLabel lblContrasenya = new JLabel("Contraseña: ");
+        lblContrasenya.setFont(new Font("URIAL FONT", Font.BOLD, 13));
+        //lblContrasenya.setForeground(new Color(255, 170, 0));
+
+
 
         JButton btnInvitado = new JButton("Entrar como invitado");
+        btnInvitado.setFont(new Font("URIAL FONT", Font.BOLD, 13));
+        btnInvitado.setForeground(new Color(255,69,0));
+        btnInvitado.setBackground(new Color(255, 170, 0));
         JButton btnCrearUsuario = new JButton(" Crear nuevo usuario");
+        btnCrearUsuario.setFont(new Font("URIAL FONT", Font.BOLD, 13));
+        btnCrearUsuario.setForeground(new Color(255,69,0));
+        btnCrearUsuario.setBackground(new Color(255, 170, 0));
         JButton btnLogin = new JButton("Login");
+        btnLogin.setFont(new Font("URIAL FONT", Font.BOLD, 13));
+        btnLogin.setForeground(new Color(255,69,0));
+        btnLogin.setBackground(new Color(255, 170, 0));
+        btnLogin.setBackground(new Color(255, 170, 0));
+
 
         JTextField txtUsuario = new JTextField(15);
         JTextField txtContrasenya = new JTextField(15);
 
         JPanel pnlNorte = new JPanel(new FlowLayout());
+        pnlNorte.setBackground(new Color(239, 127, 26));
         JPanel pnlCentro = new JPanel(new FlowLayout());
+        pnlCentro.setBackground(new Color(239, 127, 26));
+
 
         pnlNorte.add(lblUsuario);
         pnlNorte.add(txtUsuario);
